77bb911225bea2871019d9dca8f727b1c897cd2a
Darcy <github@darcythomas.com>
2015-06-29 11:38:17 +1200

    Set up autofac

--- RetailNews/RetailNews/Startup.cs
+++ RetailNews/RetailNews/Startup.cs
@@ -1,5 +1,13 @@
-﻿using Microsoft.Owin;
+﻿using System.Web;
+using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.DataProtection;
 using Owin;
+using RetailNews.Models;
 
 [assembly: OwinStartupAttribute(typeof(RetailNews.Startup))]
 namespace RetailNews
@@ -8,6 +16,29 @@ namespace RetailNews
     {
         public void Configuration(IAppBuilder app)
         {
+            var builder = new ContainerBuilder();
+
+            // REGISTER DEPENDENCIES
+            builder.RegisterType<ApplicationDbContext>().AsSelf().InstancePerRequest();
+            builder.RegisterType<ApplicationUserStore>().As<IUserStore<ApplicationUser>>().InstancePerRequest();
+            builder.RegisterType<ApplicationUserManager>().AsSelf().InstancePerRequest();
+            builder.RegisterType<ApplicationSignInManager>().AsSelf().InstancePerRequest();
+            builder.Register<IAuthenticationManager>(c => HttpContext.Current.GetOwinContext().Authentication).InstancePerRequest();
+            builder.Register<IDataProtectionProvider>(c => app.GetDataProtectionProvider()).InstancePerRequest();
+
+            // REGISTER CONTROLLERS SO DEPENDENCIES ARE CONSTRUCTOR INJECTED
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            // BUILD THE CONTAINER
+            var container = builder.Build();
+
+            // REPLACE THE MVC DEPENDENCY RESOLVER WITH AUTOFAC
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+            // REGISTER WITH OWIN
+            app.UseAutofacMiddleware(container);
+            app.UseAutofacMvc();
+
             ConfigureAuth(app);
         }
     }
